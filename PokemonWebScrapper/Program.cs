fe5cdd034e9d4a9481d4f70af6a3315800df9a75
Sergio Triana Escobedo <stescobedo.31@gmail.com>
2024-03-15 09:59:55 -0600

    Adding doc

--- PokemonWebScrapper/Program.cs
+++ PokemonWebScrapper/Program.cs
@@ -12,6 +12,16 @@ namespace PokemonWebScrapper
             Scraper();
         }
 
+        /// <summary>
+        /// Scrapes data from a website and exports it to a CSV file.
+        /// </summary>
+        /// <remarks>
+        /// This method scrapes product data from the specified website and exports it to a CSV file.
+        /// </remarks>
+        /// <seealso cref="HtmlWeb"/>
+        /// <seealso cref="HtmlDocument"/>
+        /// <seealso cref="PokemonProduct"/>
+        /// <seealso cref="ExportDataToCsv(List{PokemonProduct})"/>
         private static void Scraper() 
         {
             var web = new HtmlWeb();
@@ -24,6 +34,18 @@ namespace PokemonWebScrapper
             pokemonProducts = ProductHtmlElements(productHTMLElements);
             ExportDataToCsv(pokemonProducts: pokemonProducts);
         }
+
+        /// <summary>
+        /// Extracts Pokemon product information from HTML elements.
+        /// </summary>
+        /// <param name="productHTMLElements">The HTML elements representing Pokemon products.</param>
+        /// <returns>A list of PokemonProduct objects extracted from the HTML elements, or null if the input is null.</returns>
+        /// <remarks>
+        /// This method takes a collection of HTML elements representing Pokemon products and extracts relevant information such as URL, image, name, and price.
+        /// It returns a list of PokemonProduct objects containing this information.
+        /// </remarks>
+        /// <seealso cref="HtmlNode"/>
+        /// <seealso cref="PokemonProduct"/>
         private static List<PokemonProduct>? ProductHtmlElements(IList<HtmlNode>? productHTMLElements)
         {
             List<PokemonProduct> pokemonProducts = new();
@@ -43,6 +65,15 @@ namespace PokemonWebScrapper
             return pokemonProducts;
         }
 
+        /// <summary>
+        /// Exports a list of Pokemon products to a CSV file.
+        /// </summary>
+        /// <param name="pokemonProducts">The list of Pokemon products to export.</param>
+        /// <remarks>
+        /// This method exports the provided list of Pokemon products to a CSV file named "pokemon-products.csv".
+        /// Each Pokemon product is represented as a row in the CSV file, with columns for URL, image, name, and price.
+        /// </remarks>
+        /// <seealso cref="PokemonProduct"/>
         private static void ExportDataToCsv(List<PokemonProduct>? pokemonProducts)
         {
             using (var writer = new StreamWriter("pokemon-products.csv"))
