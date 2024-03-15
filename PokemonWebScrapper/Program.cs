63af4e3e0f4602557c57c2cec5b9cbda4c2b4711
Sergio Triana Escobedo <stescobedo.31@gmail.com>
2024-03-15 09:54:17 -0600

    Adding scrapping methods

--- PokemonWebScrapper/Program.cs
+++ PokemonWebScrapper/Program.cs
@@ -1,10 +1,56 @@
-﻿namespace PokemonWebScrapper
+﻿using CsvHelper;
+using HtmlAgilityPack;
+using PokemonWebScrapper.Model;
+using System.Globalization;
+
+namespace PokemonWebScrapper
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Scraper();
+        }
+
+        private static void Scraper() 
+        {
+            var web = new HtmlWeb();
+            var document = web.Load("https://scrapeme.live/shop/");
+            var pokemonProducts = new List<PokemonProduct>();
+
+            // selecting all HTML product elements from the current page 
+            var productHTMLElements = document.DocumentNode.QuerySelectorAll("li.product");
+
+            pokemonProducts = ProductHtmlElements(productHTMLElements);
+            ExportDataToCsv(pokemonProducts: pokemonProducts);
+        }
+        private static List<PokemonProduct>? ProductHtmlElements(IList<HtmlNode>? productHTMLElements)
+        {
+            List<PokemonProduct> pokemonProducts = new();
+
+            foreach (var productHTMLElement in productHTMLElements)
+            {
+                var url = HtmlEntity.DeEntitize(productHTMLElement.QuerySelector("a").Attributes["href"].Value);
+                var image = HtmlEntity.DeEntitize(productHTMLElement.QuerySelector("img").Attributes["src"].Value);
+                var name = HtmlEntity.DeEntitize(productHTMLElement.QuerySelector("h2").InnerText);
+                var price = HtmlEntity.DeEntitize(productHTMLElement.QuerySelector(".price").InnerText);
+
+                var pokemonProduct = new PokemonProduct() { Url = url, Image = image, Name = name, Price = price };
+
+                pokemonProducts.Add(pokemonProduct);
+            }
+
+            return pokemonProducts;
+        }
+
+        private static void ExportDataToCsv(List<PokemonProduct>? pokemonProducts)
+        {
+            using (var writer = new StreamWriter("pokemon-products.csv"))
+
+            using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+            {
+                csv.WriteRecords(pokemonProducts);
+            }
         }
     }
 }
