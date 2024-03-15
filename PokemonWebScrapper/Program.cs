1789aac3a936a78760b3837b48ac016595c4ad2e
Sergio Triana Escobedo <stescobedo.31@gmail.com>
2024-03-15 09:37:39 -0600

    Add project files.

---
+++ PokemonWebScrapper/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PokemonWebScrapper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
