9da774e18703b6b883a32039ac92080e7e69b8ff
Sergio Triana Escobedo <stescobedo.31@gmail.com>
2024-03-15 09:52:30 -0600

    Adding pokemon model

---
+++ PokemonWebScrapper/Model/PokemonProduct.cs
@@ -0,0 +1,9 @@
+﻿namespace PokemonWebScrapper.Model;
+
+internal class PokemonProduct
+{
+    public string? Url { get; set; }
+    public string? Image { get; set; }
+    public string? Name { get; set; }
+    public string? Price { get; set; }
+}
